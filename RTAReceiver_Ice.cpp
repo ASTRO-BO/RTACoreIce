1dc3db3fbb72c64acb0e5430dd8ed2ced1a0fecc
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-03-05 11:07:22 +0100

    Fixed licenses.

--- RTAReceiver_Ice.cpp
+++ RTAReceiver_Ice.cpp
@@ -1,14 +1,16 @@
 /***************************************************************************
- * begin                : Nov 27 2013
- * copyright            : (C) 2013 Andrea Zoli
- * email                : zoli@iasfbo.inaf.it
- * *************************************************************************
- *
- * Copyright (c) 2003-2013 ZeroC, Inc. All rights reserved.
- *
- * This copy of Ice is licensed to you under the terms described in the
- * ICE_LICENSE file included in this distribution.
- *
+    begin                : Mar 05 2014
+    copyright            : (C) 2014 Andrea Zoli
+    email                : zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
  ***************************************************************************/
 
 #include <IceUtil/IceUtil.h>
--- RTAWaveServer.cpp
+++ RTAWaveServer.cpp
@@ -1,11 +1,17 @@
-// **********************************************************************
-//
-// Copyright (c) 2003-2013 ZeroC, Inc. All rights reserved.
-//
-// This copy of Ice is licensed to you under the terms described in the
-// ICE_LICENSE file included in this distribution.
-//
-// **********************************************************************
+/***************************************************************************
+    begin                : Mar 05 2014
+    copyright            : (C) 2014 Andrea Zoli
+    email                : zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
 
 #include <IceUtil/IceUtil.h>
 #include <Ice/Ice.h>
